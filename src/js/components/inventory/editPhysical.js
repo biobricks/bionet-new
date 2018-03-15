61865c9bab3736efddeacb86737e353604e526aa
autonomous <autonomous666@gmail.com>
2018-03-14 17:46:17 -0700

    selected item just created in inventory path

--- src/js/components/inventory/editPhysical.js
+++ src/js/components/inventory/editPhysical.js
@@ -115,7 +115,7 @@ module.exports = function (Component) {
                     return
                 }
                 app.actions.notify(dbData.name+" saved", 'notice', 2000);
-                app.actions.inventory.selectInventoryId(dbData.parent_id)
+                app.actions.inventory.selectInventoryId(id)
             })
             
         }

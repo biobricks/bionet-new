fc7b8deb14f369c9838525a215b3111cca88d5e0
autonomous <autonomous666@gmail.com>
2018-03-28 13:41:48 -0700

    added tests for MoveItem component

--- tests/components/inventory/index.js
+++ tests/components/inventory/index.js
@@ -2,4 +2,5 @@ describe('Inventory component tests', () => {
     require('./favoritesTest.js')
     require('./itemTypesTest.js')
     require('./storageCellTest.js')
+    require('./moveItemTest.js')
 });

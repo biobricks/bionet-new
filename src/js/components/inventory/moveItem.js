2150913ed87b4ab8d261710eba2a0506bb3bf54f
autonomous <autonomous666@gmail.com>
2018-03-28 13:41:12 -0700

    updated props for MoveItem component

--- src/js/components/inventory/moveItem.js
+++ src/js/components/inventory/moveItem.js
@@ -17,9 +17,9 @@ module.exports = function (Component) {
         moveButtonClick() {
             const container = app.actions.inventory.getLastPathItem()
             const selection = app.state.inventory.selection
-            const moveItem = app.state.inventory.moveItem
+            const moveItem = this.props.item
+            //const moveItem = app.state.inventory.moveItem
             console.log('move button:',selection.x, selection.y)
-            //return
             if (moveItem && selection) {
                 moveItem.parent_id = container.id
                 moveItem.parent_x = selection.x
@@ -38,21 +38,21 @@ module.exports = function (Component) {
             }
         }
         
+        close() {
+            app.actions.inventory.setMoveItem(null)
+        }
+        
         render() {
-            const close=function() {
-                app.actions.inventory.setMoveItem(null)
-            }
-            if (!this.props.moveId) return null
-            const parent_item = app.state.inventory.moveItem
+            if (!this.props.item) return null
+            const item = this.props.item
             var storageContainer = null
-            
             const isActive = (this.state.moveActive) ? 'is-active' : ''
-
+            
             return (
-                <a id={this.props.moveId} class="navbar-item is-dark" style="background-color:#404040;">
-                    <button class="button is-small is-light" aria-haspopup="true" aria-controls="dropdown-menu-move" style="color:#000000;" onclick={this.moveButtonClick}>Move:</button>
-                    <span style="color:#ffffff;margin-left:5px; margin-right:20px;font-weight:800;">{this.props.name}</span>
-                    <span class="button is-rounded" style="background-color:rgb(64,64,64);border:none;" onclick={close}><a class="mdi mdi-close-box mdi-24px mdi-light" style="color:#000000;font-weight:800;"></a></span>
+                <a id={item.id} className="navbar-item is-dark" style="background-color:#404040;">
+                    <button className="button is-small is-light" aria-haspopup="true" aria-controls="dropdown-menu-move" style="color:#000000;" onclick={this.moveButtonClick}>Move:</button>
+                    <span style="color:#ffffff;margin-left:5px; margin-right:20px;font-weight:800;">{item.name}</span>
+                    <span className="button is-rounded" style="background-color:rgb(64,64,64);border:none;" onclick={this.close.bind(this)}><a className="mdi mdi-close-box mdi-24px mdi-light" style="color:#000000;font-weight:800;"></a></span>
                 </a>
             )
         }

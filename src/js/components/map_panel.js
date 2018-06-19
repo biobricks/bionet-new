74b3a5ca82fde177bb2687672220d24822021fa5
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-06-19 01:09:50 -0700

    update map panel

--- src/js/components/map_panel.js
+++ src/js/components/map_panel.js
@@ -6,33 +6,15 @@ module.exports = function(Component) {
   return class MapPanel extends Component {
 	  render() {
       return (
-        <div class="DataPanel panel has-background-white">
+        <div class="MapPanel panel has-background-white">
           <div className="panel-heading">
-            <div class="is-block">
-              <div class="columns is-gapless">
-                <div class="column has-text-centered-mobile">
-                  Map Panel
-                </div>
-                <div class="column is-clearfix">
-                  <div class="toolbox">
-                    <div class="buttons has-addons">
-                      <span class="button is-small is-success">
-                        <i class="mdi mdi-plus"></i>
-                      </span>
-                      <span class="button is-small is-link">
-                        <i class="mdi mdi-pencil"></i>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>    
-            </div>
-          </div>
-          <div className="panel-block">
-            Status
+            Map Panel
           </div>
           <div className="panel-block">
             Map Area
+          </div>          
+          <div className="panel-block">
+            Status
           </div>          
         </div>
       )

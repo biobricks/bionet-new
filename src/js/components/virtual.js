3b070a7307dfe68cc6683e026a43cc8aec8639dd
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-06-26 21:45:05 -0700

    update virtual styles

--- src/js/components/virtual.js
+++ src/js/components/virtual.js
@@ -111,6 +111,151 @@ module.exports = function(Component) {
     }
 
     componentDidMount() {
+      // test ui function
+      // let virtual = {
+      //   "type": "vector",
+      //   "name": "tb1_v1",
+      //   "sequence": "atgv",
+      //   "content":"markdown content here...",
+      //   "description":"plain text content here",
+      //   "id": "v-30a940c1-1730-4a6e-8c5b-b5017bc4ad7a",
+      //   "created": {
+      //     "user": "tsakach",
+      //     "time": 1524515914
+      //   },
+      //   "updated": {
+      //     "user": "tsakach",
+      //     "time": 1524515914
+      //   }
+      // };
+
+      // let physicals = [  
+      //   {
+      //     "name": "tb1_v1_1",
+      //     "type": "physical",
+      //     "virtual_id": "v-30a940c1-1730-4a6e-8c5b-b5017bc4ad7a",
+      //     "parent_id": "p-bb936051-a9ba-4bbd-a25d-a3eb84fec53e",
+      //     "parent_x": 3,
+      //     "parent_y": 2,
+      //     "id": "p-01a59225-9902-41e4-a154-e4d7af71e737",
+      //     "created": {
+      //       "user": "tsakach",
+      //       "time": 1524515914
+      //     },
+      //     "updated": {
+      //       "user": "tsakach",
+      //       "time": 1525310871
+      //     }
+      //   },
+      //   {
+      //     "name": "tb1_v1_6",
+      //     "type": "physical",
+      //     "virtual_id": "v-30a940c1-1730-4a6e-8c5b-b5017bc4ad7a",
+      //     "parent_id": "p-21df6ff3-cb64-41c2-9ba7-cfe878c7acce",
+      //     "parent_x": 6,
+      //     "parent_y": 6,
+      //     "id": "p-1ace3cd6-619a-4688-9749-91a53ed49653",
+      //     "created": {
+      //       "user": "tsakach",
+      //       "time": 1524515914
+      //     },
+      //     "updated": {
+      //       "user": "tsakach",
+      //       "time": 1525122739
+      //     }
+      //   },
+      //   {
+      //     "name": "tb1_v1_2",
+      //     "type": "physical",
+      //     "virtual_id": "v-30a940c1-1730-4a6e-8c5b-b5017bc4ad7a",
+      //     "parent_id": "p-bb936051-a9ba-4bbd-a25d-a3eb84fec53e",
+      //     "parent_x": 3,
+      //     "parent_y": 1,
+      //     "id": "p-3ae79719-9f10-4611-907a-d1f1704be458",
+      //     "created": {
+      //       "user": "tsakach",
+      //       "time": 1524515914
+      //     },
+      //     "updated": {
+      //       "user": "tsakach",
+      //       "time": 1524515914
+      //     }
+      //   },
+      //   {
+      //     "name": "tb1_v1_0",
+      //     "type": "physical",
+      //     "virtual_id": "v-30a940c1-1730-4a6e-8c5b-b5017bc4ad7a",
+      //     "parent_id": "p-bb936051-a9ba-4bbd-a25d-a3eb84fec53e",
+      //     "parent_x": 1,
+      //     "parent_y": 1,
+      //     "id": "p-6901db85-00dc-4038-a363-29b54b4ee203",
+      //     "created": {
+      //       "user": "tsakach",
+      //       "time": 1524515914
+      //     },
+      //     "updated": {
+      //       "user": "tsakach",
+      //       "time": 1524515914
+      //     }
+      //   },
+      //   {
+      //     "name": "tb1_v1_5",
+      //     "type": "physical",
+      //     "virtual_id": "v-30a940c1-1730-4a6e-8c5b-b5017bc4ad7a",
+      //     "parent_id": "p-21df6ff3-cb64-41c2-9ba7-cfe878c7acce",
+      //     "parent_x": 8,
+      //     "parent_y": 4,
+      //     "id": "p-92fbcb4b-57c0-4621-8dba-ed492eb66e0f",
+      //     "created": {
+      //       "user": "tsakach",
+      //       "time": 1524515914
+      //     },
+      //     "updated": {
+      //       "user": "tsakach",
+      //       "time": 1525300960
+      //     }
+      //   },
+      //   {
+      //     "name": "tb1_v1_4",
+      //     "type": "physical",
+      //     "virtual_id": "v-30a940c1-1730-4a6e-8c5b-b5017bc4ad7a",
+      //     "parent_id": "p-bb936051-a9ba-4bbd-a25d-a3eb84fec53e",
+      //     "parent_x": 5,
+      //     "parent_y": 1,
+      //     "id": "p-bd1e2d13-d8ce-43b1-b556-dc7137377e5d",
+      //     "created": {
+      //       "user": "tsakach",
+      //       "time": 1524515914
+      //     },
+      //     "updated": {
+      //       "user": "tsakach",
+      //       "time": 1524515914
+      //     }
+      //   },
+      //   {
+      //     "name": "tb1_v1_3",
+      //     "type": "physical",
+      //     "virtual_id": "v-30a940c1-1730-4a6e-8c5b-b5017bc4ad7a",
+      //     "parent_id": "p-bb936051-a9ba-4bbd-a25d-a3eb84fec53e",
+      //     "parent_x": 4,
+      //     "parent_y": 1,
+      //     "id": "p-ed37c7eb-e057-4fa9-80a4-8f5a71ab8d1b",
+      //     "created": {
+      //       "user": "tsakach",
+      //       "time": 1524515914
+      //     },
+      //     "updated": {
+      //       "user": "tsakach",
+      //       "time": 1524515914
+      //     }
+      //   }
+      // ];
+      // this.setState({
+      //   virtual,
+      //   physicals,
+      //   error: null,
+      //   loading: false
+      // });
     }
 
 	  render() {
@@ -177,28 +322,27 @@ module.exports = function(Component) {
         }
 
         virtual = (
-          <div class="virtual">
-            <div>
-              <h3>{this.state.virtual.name} {modifyLinks}</h3>
+          <div class="panel-block">
+            <div class="virtual">
+              <div>
+                <p class="description">{this.state.virtual.description}</p>
+              </div>
+              {content}
+              <div>
+                <span style="font-weight:bold">Provenance:</span> {this.state.virtual.provenance || "Unknown"}
+              </div>
+              <div>
+                <span style="font-weight:bold">Genotype:</span> {this.state.virtual.genotype || "None"}
+              </div>
+              <div>
+                <span style="font-weight:bold">Sequence:</span> {this.state.virtual.sequence || "None"}
+              </div>
+              <div>
+                <span style="font-weight:bold">Terms and condition:</span> {this.state.virtual.terms || "Limbo"}
+              </div>
+              <div>{timestamps}</div>
+              <div>{sequence}</div>
             </div>
-            <div>
-              <p class="description">{this.state.virtual.description}</p>
-            </div>
-            {content}
-            <div>
-              <span style="font-weight:bold">Provenance:</span> {this.state.virtual.provenance || "Unknown"}
-            </div>
-            <div>
-              <span style="font-weight:bold">Genotype:</span> {this.state.virtual.genotype || "None"}
-            </div>
-            <div>
-              <span style="font-weight:bold">Sequence:</span> {this.state.virtual.sequence || "None"}
-            </div>
-            <div>
-              <span style="font-weight:bold">Terms and condition:</span> {this.state.virtual.terms || "Limbo"}
-            </div>
-            <div>{timestamps}</div>
-            <div>{sequence}</div>
           </div>
         )
       }
@@ -216,29 +360,58 @@ module.exports = function(Component) {
       let i;
       for(i=0; i < this.state.physicals.length; i++) {
         physicals.push((
-          <ul>
-            <li><Link to={'/inventory/'+this.state.physicals[i].id}>{this.state.physicals[i].name}</Link></li>
-          </ul>
+          <Link 
+            to={'/inventory/'+this.state.physicals[i].id}
+            class="panel-block"
+          >
+            <i class="mdi mdi-flask has-text-success"></i>&nbsp;&nbsp;{this.state.physicals[i].name}
+          </Link>
         ));
       }
 
 
       if(!physicals.length) {
         physicals = (
-          <div><p>No physical instances</p></div>
+          <div class="panel-block"><p>No physical instances</p></div>
         )
       }
 
       return (
-        <div>
-          {status}
-          <div>
-            {virtual}
+        <div class="Virtual panel">
+          <div class="panel-heading">
+            <span>{this.state.virtual ? this.state.virtual.name : "Loading"}</span>
+            {(this.state.virtual) ? (
+              <div class="toolbox is-pulled-right">
+                <div class="buttons has-addons">
+                  <Link 
+                    class="button is-small is-link"
+                    to={'/virtual/edit/' + this.state.virtual.id}
+                  >
+                    <i class="mdi mdi-pencil"></i>
+                  </Link>
+                  <a 
+                    href=""
+                    class="button is-small is-danger"
+                    onClick={this.delVirtual.bind(this)}
+                  >
+                    <i class="mdi mdi-delete-variant"></i>
+                  </a>
+                </div>
+              </div>   
+            ) : null }         
           </div>
-          <div>
-            <h3>Physical instances:</h3>
-            {physicals}
+          {(this.state.virtual && this.state.virtual.freegenes) ? (
+            <div class="panel-block">
+              {status}
+            </div>              
+          ) : null }
+        
+          {virtual}
+        
+          <div class="panel-block">
+            <h5>Physical Instances</h5>
           </div>
+          {physicals}
           <div>
             {loading}
           </div>

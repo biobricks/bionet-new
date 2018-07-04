96721fdc6bd259a5912ab4e30a3945bb0ea723f3
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-07-04 10:41:29 -0700

    update edit physical form fields

--- src/js/components/demo/PhysicalEditForm.js
+++ src/js/components/demo/PhysicalEditForm.js
@@ -19,7 +19,7 @@ module.exports = function(Component) {
                   </div>
                   <div class="column">   
                     <input 
-                      class="input  "
+                      class="input"
                       type="text" 
                       value={selectedRecord.name}
                     />
@@ -27,51 +27,75 @@ module.exports = function(Component) {
                 </div>
               </div>
               <div class="column is-12">
-                <div class="columns is-gapless">
+                <div class="columns">
                   <div class="column is-narrow">
                     <label class="label">Description</label>
                   </div>
                   <div class="column">   
                     <textarea 
-                      class="textarea  " 
+                      class="textarea" 
                       value={selectedRecord.description}
                       rows="2"
                     >{selectedRecord.description}</textarea>
                   </div>
                 </div>
-              </div>              
+              </div>
               <div class="column is-12">
-                <div class="columns is-mobile">
+                <div class="columns">
                   <div class="column is-narrow">
-                    <label class="label">Location</label>
+                    <label class="label">Is Available?</label>
+                  </div>
+                  <div class="column">   
+                    <div class="control">
+                      <label class="radio">
+                        <input type="radio" name="available" />
+                        &nbsp;Yes
+                      </label>
+                      <label class="radio">
+                        <input type="radio" name="available" checked/>
+                        &nbsp;No
+                      </label>
+                    </div>
                   </div>
                 </div>
               </div>
               <div class="column is-12">
-                <div class="columns is-gapless">
+                <div class="columns">
                   <div class="column is-narrow">
-                    <label class="label">Row</label>
+                    <label class="label">Submitted To FreeGenes?</label>
                   </div>
                   <div class="column">   
-                    <input 
-                      class="input  "
-                      type="number" 
-                      value={selectedRecord.row}
-                    />
+                    <div class="control">
+                      <label class="radio">
+                        <input type="radio" name="freeGenes" />
+                        &nbsp;Yes
+                      </label>
+                      <label class="radio">
+                        <input type="radio" name="freeGenes" checked/>
+                        &nbsp;No
+                      </label>
+                    </div>
                   </div>
                 </div>
               </div>
               <div class="column is-12">
-                <div class="columns is-gapless">
+                <div class="columns">
                   <div class="column is-narrow">
-                    <label class="label">Column</label>
+                    <label class="label">FreeGenes Stage</label>
                   </div>
                   <div class="column">   
-                    <input 
-                      class="input  "
-                      type="number" 
-                      value={selectedRecord.column}
-                    />
+                    <div class="select" name="freeGenesStage">
+                      <select>
+                        <option value="0">0 - Not Submitted</option>
+                        <option value="1">1 - Submitted</option>
+                        <option value="2">2 - Optimizing</option>
+                        <option value="3">3 - Synthesizing</option>
+                        <option value="4">4 - Cloning</option>
+                        <option value="5">5 - Sequencing</option>
+                        <option value="6">6 - Shipping</option>
+                        <option value="7">7 - Delivered</option>
+                      </select>
+                    </div>
                   </div>
                 </div>
               </div>              

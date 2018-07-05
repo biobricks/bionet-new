cf42eadef963a3401a99abc984d00308ed396a3d
autonomous <autonomous666@gmail.com>
2018-07-04 17:25:59 -0700

    added save updates to new physical form

--- src/js/components/physical_new_form.js
+++ src/js/components/physical_new_form.js
@@ -3,7 +3,37 @@ import { h } from 'preact';
 module.exports = function(Component) {
 
   return class PhysicalNewForm extends Component {
-
+      
+    constructor(props, context) {
+        super(props, context)
+        this.state={
+            name:'',
+            description:'',
+            content:'',
+            instances:1
+        }
+    }
+    update(newProps) {
+        const newState = Object.assign(this.state, newProps)
+        this.setState(newState,()=>{/*console.log('PhysicalNew update:',this.state)*/})
+        if (this.props.onChange) this.props.onChange(newProps)
+    }
+    onName(e) {
+        this.update({name:e.target.value})
+    }
+    onDescription(e) {
+        this.update({description:e.target.value})
+    }
+    onContent(e) {
+        this.update({content:e.target.value})
+    }
+    onInstances(e) {
+        this.update({instances:e.target.value})
+    }
+    onSave() {
+        console.log('PhysicalNew onSave:',this.state)
+        if (this.props.onSave) this.props.onSave(this.state)
+    }
     render() {
       let selectedRecord = this.props.selectedRecord;
       let parentRecord = this.props.parentRecord;
@@ -29,6 +59,8 @@ module.exports = function(Component) {
                       class="input is-small"
                       type="text" 
                       placeholder="Physical Name"
+                      onChange={this.onName.bind(this)}
+                      value={this.state.name} 
                     />
                   </div>
                 </div>
@@ -43,6 +75,8 @@ module.exports = function(Component) {
                       class="textarea is-small"
                       rows="2"
                       placeholder="A short description of the Physical."
+                      onChange={this.onDescription.bind(this)}
+                      value={this.state.description} 
                     ></textarea>
                   </div>
                 </div>
@@ -50,13 +84,31 @@ module.exports = function(Component) {
               <div class="column is-12">
                 <div class="columns is-gapless">
                   <div class="column is-narrow">
-                    <label class="label">Row</label>
+                    <label class="label">Content</label>
+                  </div>
+                  <div class="column">   
+                    <textarea 
+                      class="textarea is-small"
+                      rows="2"
+                      placeholder="Markdown content."
+                      onChange={this.onContent.bind(this)}
+                      value={this.state.content} 
+                    ></textarea>
+                  </div>
+                </div>
+              </div>              
+              <div class="column is-12">
+                <div class="columns is-gapless">
+                  <div class="column is-narrow">
+                    <label class="label">Instances</label>
                   </div>
                   <div class="column">   
                     <input 
                       class="input is-small"
                       type="number" 
                       min="1"
+                      onChange={this.onInstances.bind(this)}
+                      value={this.state.instances} 
                     />
                   </div>
                 </div>
@@ -64,17 +116,11 @@ module.exports = function(Component) {
               <div class="column is-12">
                 <div class="columns is-gapless">
                   <div class="column is-narrow">
-                    <label class="label">Column</label>
-                  </div>
-                  <div class="column">   
-                    <input 
-                      class="input is-small"
-                      type="number" 
-                      min="1"
-                    />
+                  <button onClick={this.onSave.bind(this)} >Save</button>
                   </div>
                 </div>
-              </div>              
+              </div>
+    
             </div> 
           </div>
         </div>

aa1fcbd9841c8bfe06fcab09335ff548f611cc64
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-07-10 19:07:29 -0700

    add map type toggle

--- src/js/components/demo/MapPanel.js
+++ src/js/components/demo/MapPanel.js
@@ -7,38 +7,104 @@ module.exports = function(Component) {
 
   return class MapPanel extends Component {
 
+    constructor(props) {
+      super(props);
+      this.state = {
+        mapType: 'end to end'
+      };
+      this.toggleMapType = this.toggleMapType.bind(this);
+    }
+
+    toggleMapType(e) {
+      let mapType = e.target.getAttribute('name');
+      console.log(`switched to ${mapType}`);
+      this.setState({ mapType });
+    }
+
 	  render() {
       let record = this.props.selectedRecord;
       let isContainer = Object.keys(record).indexOf('children') > -1;
-      let isEditMode = this.props.editMode;
-      let isNewMode = this.props.newMode;    
+          
       return (
         <div class="MapPanel panel has-background-white">
-          <div className="panel-heading">
-            {(isContainer) ? (
-              <span>{this.props.selectedRecord.name}</span>
-            ) : null }
-            {(!isContainer) ? (
-              <span>{this.props.parentRecord.name}</span>
-            ) : null }
+          
+          <div class="panel-heading is-capitalized">
+            <div class="is-block">
+
+              {(isContainer) ? (
+                <span><i class="mdi mdi-grid"></i> {this.props.selectedRecord.name}</span>
+              ) : null }
+              {(!isContainer) ? (
+                <span><i class="mdi mdi-flask"></i> {this.props.parentRecord.name}</span>
+              ) : null }
+
+              {(this.state.mapType === 'end to end') ? (
+                <div class="toolbox is-pulled-right">
+                  <div class="buttons has-addons">
+                    <span 
+                      class="button is-small is-primary"
+                      name="end to end"
+                      onClick={this.toggleMapType}
+                      disabled
+                    >
+                      <i class="mdi mdi-view-sequential" name="end to end"></i>
+                    </span>
+                    <span 
+                      class="button is-small is-dark"
+                      name="free form"
+                      onClick={this.toggleMapType}
+                    >
+                      <i class="mdi mdi-view-quilt" name="free form"></i>
+                    </span>
+                  </div>
+                </div>
+              ) : (
+                <div class="toolbox is-pulled-right">
+                  <div class="buttons has-addons">
+                    <span 
+                      class="button is-small is-dark"
+                      name="end to end"
+                      onClick={this.toggleMapType}
+                    >
+                      <i class="mdi mdi-view-sequential" name="end to end"></i>
+                    </span>
+                    <span 
+                      class="button is-small is-primary"
+                      name="free form"
+                      onClick={this.toggleMapType}
+                      disabled
+                    >
+                      <i class="mdi mdi-view-quilt" name="free form"></i>
+                    </span>
+                  </div>
+                </div>
+              )}              
+
+            </div>
           </div>
-          <div class="map-container">
-            {(isContainer) ? (
-              <MapGrid 
-                {...this.props}
-                record={this.props.selectedRecord}
-                selectRecord={this.props.selectRecord}
-              />
-            ) : null }
-            {(!isContainer) ? (
-              <MapGrid 
-                {...this.props}
-                record={this.props.parentRecord}
-                selectRecord={this.props.selectRecord}
-              />
-            ) : null }              
-          </div>
-                 
+          
+          {(this.state.mapType === 'end to end') ? (
+            <div class="map-container">
+              {(isContainer) ? (
+                <MapGrid 
+                  {...this.props}
+                  record={this.props.selectedRecord}
+                  selectRecord={this.props.selectRecord}
+                />
+              ) : null }
+              {(!isContainer) ? (
+                <MapGrid 
+                  {...this.props}
+                  record={this.props.parentRecord}
+                  selectRecord={this.props.selectRecord}
+                />
+              ) : null }              
+            </div>
+          ) : (
+            <div class="map-container">
+              Free Form Component Goes Here
+            </div>
+          )}      
         </div>
       )
     }

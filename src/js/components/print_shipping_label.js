b7f3798a68f20ac37b7d7e63d212147399a318f8
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-07-31 11:04:36 -0700

    add panel layout

--- src/js/components/print_shipping_label.js
+++ src/js/components/print_shipping_label.js
@@ -57,81 +57,82 @@ module.exports = function(Component) {
       }
 
       return (
-
         <div class="print-shipping-label">
-          {error}
-          <div>
-            <form onsubmit={this.print.bind(this)}>
-            <div class="container post-hero-area">
-              <div class="columns">
-                <div class="column is-6">
-                  <div class="field">
-                    <label class="label">Name</label>
-                    <div class="control">
-                      <input class="input" type="text" onChange={linkState(this, 'name')} />
-                    </div>
-                  </div>
-                  <div class="field">
-                    <label class="label">Steet address</label>
-                    <div class="control">
-                      <input class="input" type="text" onChange={linkState(this, 'street1')} />
-                    </div>
-                  </div>
-                  <div class="field">
-                    <label class="label">Steet address 2</label>
-                    <div class="control">
-                      <input class="input" type="text" onChange={linkState(this, 'street2')} />
-                    </div>
-                  </div>
-                  <div class="field">
-                    <label class="label">Company or organization</label>
-                    <div class="control">
-                      <input class="input" type="text" onChange={linkState(this, 'company')} />
-                    </div>
-                  </div>
-                  <div class="field">
-                    <label class="label">City</label>
-                    <div class="control">
-                      <input class="input" type="text" onChange={linkState(this, 'city')} />
-                    </div>
-                  </div>
-                  <div class="field">
-                    <label class="label">Zipcode</label>
-                    <div class="control">
-                      <input class="input" type="text" onChange={linkState(this, 'zip')} />
-                    </div>
-                  </div>
-                  <div class="field">
-                    <label class="label">State</label>
-                    <div class="control">
-                      <input class="input" type="text" onChange={linkState(this, 'state')} />
-                    </div>
-                  </div>
-                  <div class="field">
-                    <label class="label">Email</label>
-                    <div class="control has-icons-left">
-                      <input class="input" type="text" onChange={linkState(this, 'email')} />
-                      <span class="icon is-small is-left">
-                        <i class="fa fa-envelope"></i>
-                      </span>
-                    </div>
-                  </div>
-
-
-            <div class="field is-grouped">
-              <div class="control">
-                <input type="submit" class="button is-link" value="Print shipping label" />
-              </div>
-              <div class="control">
-                <button class="button is-text" onClick={this.cancel.bind(this)}>Cancel</button>
+          <div class="columns">
+            <div class="column is-6-desktop">
+              <div class="panel mt-1">
+                <div class="panel-heading">
+                  Print Shipping Label
                 </div>
+
+                <div class="panel-block">
+                  {error} 
+                  <form onsubmit={this.print.bind(this)}>
+                    <div class="field">
+                      <label class="label">Name</label>
+                      <div class="control">
+                        <input class="input" type="text" onChange={linkState(this, 'name')} />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">Street Address</label>
+                      <div class="control">
+                        <input class="input" type="text" onChange={linkState(this, 'street1')} />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">Street Address 2</label>
+                      <div class="control">
+                        <input class="input" type="text" onChange={linkState(this, 'street2')} />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">Company / Organization</label>
+                      <div class="control">
+                        <input class="input" type="text" onChange={linkState(this, 'company')} />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">City</label>
+                      <div class="control">
+                        <input class="input" type="text" onChange={linkState(this, 'city')} />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">Zipcode</label>
+                      <div class="control">
+                        <input class="input" type="text" onChange={linkState(this, 'zip')} />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">State</label>
+                      <div class="control">
+                        <input class="input" type="text" onChange={linkState(this, 'state')} />
+                      </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">Email</label>
+                      <div class="control has-icons-left">
+                        <input class="input" type="text" onChange={linkState(this, 'email')} />
+                        <span class="icon is-small is-left">
+                          <i class="fa fa-envelope"></i>
+                        </span>
+                      </div>
+                    </div>
+                    <div class="field is-grouped mt-1">
+                      <div class="control">
+                        <input type="submit" class="button is-link" value="Print shipping label" />
+                      </div>
+                      <div class="control">
+                        <button class="button is-text" onClick={this.cancel.bind(this)}>Cancel</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>    
             </div>
-                 </div>
-               </div>
-              </div>
-            </form>
           </div>
-        </div>
+        </div>          
       )
     }
   }

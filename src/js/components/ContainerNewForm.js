98493d1c3f540c268af90ad838fa3919113661a6
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-08-22 22:28:20 -0700

    update new container form

--- src/js/components/ContainerNewForm.js
+++ src/js/components/ContainerNewForm.js
@@ -33,12 +33,15 @@ module.exports = function(Component) {
           description: '',
           xUnits: 1,
           yUnits: 1,
-          color: 'aqua',
+          fontSize: 16,
+          color: '#00FFFF',
           majorGridLine: 1,
           viewOrientation: 'top',
           parent_id: '',
           parent_x: 0,
-          parent_y: 0
+          parent_y: 0,
+          parent_x_span: 1,
+          parent_y_span: 1
         }
       };
       this.updateFormField = this.updateFormField.bind(this);
@@ -145,35 +148,190 @@ module.exports = function(Component) {
 
               <div class="field is-horizontal">
                 <div class="field-label is-normal is-narrow">
-                  <label class="label">Columns</label>
+                  <label class="label mt-1">Style</label>
                 </div>
                 <div class="field-body">        
-                  <input 
-                      class="input"
-                      type="number" 
-                      name="xUnits"
-                      min="1"
-                      step="1"
-                      value={this.state.form.xUnits}
-                      onInput={this.updateFormField}
-                    />
+                  <div class="field is-expanded">
+                    <div class="field has-addons">
+                      <div class="control">
+                        <a class="button is-static">
+                          Font Size
+                        </a>
+                      </div>                      
+                      <div class="control is-expanded">
+                        <input 
+                          class="input"
+                          type="number" 
+                          name="fontSize"
+                          min="8"
+                          step="1"
+                          value={this.state.form.fontSize}
+                          onInput={this.updateFormField}
+                        />
+                      </div>
+                      <div class="control">
+                        <a class="button is-static">
+                          px
+                        </a>
+                      </div>                      
+                    </div>
+                  </div>  
+                  <div class="field has-addons">
+                    <div class="control">
+                      <a class="button is-static">
+                        Background Color
+                      </a>
+                    </div>
+                    <div class="control is-expanded">
+                      <input 
+                        class="input"
+                        type="color" 
+                        name="color"
+                        min="1"
+                        step="1"
+                        value={this.state.form.color}
+                        onInput={this.updateFormField}
+                      />
+                    </div>
+                  </div>  
                 </div>
               </div>
 
               <div class="field is-horizontal">
                 <div class="field-label is-normal is-narrow">
-                  <label class="label">Rows</label>
+                  <label class="label mt-1">Internal Dimensions</label>
                 </div>
                 <div class="field-body">        
-                  <input 
-                      class="input"
-                      type="number" 
-                      name="yUnits"
-                      min="1"
-                      step="1"
-                      value={this.state.form.yUnits}
-                      onInput={this.updateFormField}
-                    />
+                  <div class="field is-expanded">
+                    <div class="field has-addons">
+                      <div class="control">
+                        <a class="button is-static">
+                          Rows
+                        </a>
+                      </div>                      
+                      <div class="control is-expanded">
+                        <input 
+                          class="input"
+                          type="number" 
+                          name="yUnits"
+                          min="1"
+                          step="1"
+                          value={this.state.form.yUnits}
+                          onInput={this.updateFormField}
+                        />
+                      </div>
+                    </div>
+                  </div>  
+                  <div class="field has-addons">
+                    <div class="control">
+                      <a class="button is-static">
+                        Columns
+                      </a>
+                    </div>                    
+                    <div class="control is-expanded">
+                      <input 
+                        class="input"
+                        type="number" 
+                        name="xUnits"
+                        min="1"
+                        step="1"
+                        value={this.state.form.xUnits}
+                        onInput={this.updateFormField}
+                      />
+                    </div>
+                  </div>  
+                </div>
+              </div>
+
+              <div class="field is-horizontal">
+                <div class="field-label is-normal is-narrow">
+                  <label class="label mt-1">Position</label>
+                </div>
+                <div class="field-body">        
+                  <div class="field is-expanded">
+                    <div class="field has-addons">
+                      <div class="control">
+                        <a class="button is-static">
+                          Row
+                        </a>
+                      </div>                      
+                      <div class="control is-expanded">
+                        <input 
+                          class="input"
+                          type="number" 
+                          name="parent_y"
+                          min="1"
+                          step="1"
+                          value={this.state.form.parent_y}
+                          onInput={this.updateFormField}
+                        />
+                      </div>
+                    </div>
+                  </div>  
+                  <div class="field has-addons">
+                    <div class="control">
+                      <a class="button is-static">
+                        Column
+                      </a>
+                    </div>
+                    <div class="control">
+                      <input 
+                        class="input"
+                        type="number" 
+                        name="parent_x"
+                        min="1"
+                        step="1"
+                        value={this.state.form.parent_x}
+                        onInput={this.updateFormField}
+                      />
+                    </div>
+                  </div>  
+                </div>
+              </div>
+
+              <div class="field is-horizontal">
+                <div class="field-label is-normal is-narrow">
+                  <label class="label mt-1">External Dimensions</label>
+                </div>
+                <div class="field-body">        
+                  <div class="field is-expanded">
+                    <div class="field has-addons">
+                      <div class="control">
+                        <a class="button is-static">
+                          Rows
+                        </a>
+                      </div>                      
+                      <div class="control is-expanded">
+                        <input 
+                          class="input"
+                          type="number" 
+                          name="parent_y_span"
+                          min="1"
+                          step="1"
+                          value={this.state.form.parent_y_span}
+                          onInput={this.updateFormField}
+                        />
+                      </div>
+                    </div>
+                  </div>  
+                  <div class="field has-addons">
+                    <div class="control">
+                      <a class="button is-static">
+                        Columns
+                      </a>
+                    </div>                    
+                    <div class="control is-expanded">
+                      <input 
+                        class="input"
+                        type="number" 
+                        name="parent_x_span"
+                        min="1"
+                        step="1"
+                        value={this.state.form.parent_x_span}
+                        onInput={this.updateFormField}
+                      />
+                    </div>
+                  </div>  
                 </div>
               </div>
 

f69238e345add1bf616bb64290c7124a6a77e74c
Matthew Stewart <matthew.stewart.mi@gmail.com>
2018-08-29 09:18:20 -0700

    update alert panel

--- src/js/components/demo/AlertPanel.js
+++ src/js/components/demo/AlertPanel.js
@@ -7,23 +7,14 @@ module.exports = function(Component) {
     render() {
       return (
         <div class="AlertPanel">
-          <div class="columns is-centered">
-            
-            <div class="column is-12-mobile is-8-tablet is-6-desktop">
-              <div class="panel">
-                <div class="panel-heading has-text-centered">
-                  <h3>{this.props.heading || 'Alert Panel (props.heading)'}</h3>
-                </div>
-                <div class="panel-block">
-                  <div class="has-text-centered" style={{'margin': '0 auto'}}>
-                    <p>
-                      {this.props.message || 'Alert Panel Message. (props.message)'}
-                    </p>
-                  </div>
-                </div>
-              </div>
+          <div class="panel-heading has-text-centered">
+            <h3>{this.props.heading || 'Alert Panel (props.heading)'}</h3>
+          </div>
+          <div class="panel-block">
+            <div class="has-text-centered" style={{'margin': '0 auto'}} dangerouslySetInnerHTML={{__html: this.props.message || 'Alert Panel Message. (props.message)'}}>
+
             </div>
-          </div>    
+          </div>
         </div>
       )
     }
